aa33dc95aeac34010f3de077ea828ab97c8eb360
jtj3107 <jtj3926@gmail.com>
2021-07-26 13:42:07 +0900

    메일 발송 테스트

--- src/main/java/com/jhs/exam/exam2/http/controller/UsrHomeController.java
+++ src/main/java/com/jhs/exam/exam2/http/controller/UsrHomeController.java
@@ -1,6 +1,7 @@
 package com.jhs.exam.exam2.http.controller;
 
 import com.jhs.exam.exam2.http.Rq;
+import com.jhs.exam.exam2.util.Ut;
 
 public class UsrHomeController extends Controller {
 	@Override
@@ -9,10 +10,17 @@ public class UsrHomeController extends Controller {
 		case "main":
 			actionShowMain(rq);
 			break;
+		case "doSendMail":
+			actionDoSendMail(rq);
+			break;
 		default:
 			rq.println("존재하지 않는 페이지 입니다.");
 			break;
 		}
+	}
+
+	private void actionDoSendMail(Rq rq) {
+		Ut.sendMail("jtj3926@gmail.com", "khnvjxnxfqxotpnv", "no-reply@lemon-cm.com", "레몬 커뮤니티 알림", "jtj3926@gmail.com", "제목ㅋㅋㅋ", "내용 ㅋㅋㅋ");	
 	}
 
 	private void actionShowMain(Rq rq) {
